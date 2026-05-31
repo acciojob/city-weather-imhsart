5ba3ba598b39d7c79a431fc266ac0534a7ced5a9
imhsart <nitinrana2519@gmail.com>
2026-05-31 17:59:36 +0530

    Assignment attempt 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,40 @@
 
+import { useState, useRef } from 'react';
 import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [weatherData, setWeatherData] = useState({})
+  const [loading, setLoading] = useState(true)
+  const weatherRef = useRef('')
+
+  function handleSubmit(e){
+    e.preventDefault()
+    let val = weatherRef.current.value
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${val}&appid=de90fadc17a8c3531280d3a786567ce6`)
+    .then(res => res.json())
+    .then(data => {
+      setWeatherData(data)
+      setLoading(false)
+    })
+    .catch(error => console.log('error', error))
+
+    weatherRef.current.value = ''
+  }
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className='main'>
+        <form onSubmit={handleSubmit}>
+          <input className='search' ref={weatherRef} placeholder='Enter a city' />
+        </form>
+          {!loading && 
+          <div className='weather'>
+              <p className='name'>{weatherData.name}</p>
+              <h1>{weatherData.main.temp} °F</h1>
+              <p className='des'>{weatherData.weather[0].main}</p>
+              <img src={`https://openweathermap.org/payload/api/media/file/${weatherData.weather[0].icon}@2x.png`} />
+            </div>
+        }
     </div>
   )
 }
